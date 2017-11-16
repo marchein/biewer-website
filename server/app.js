710d676fa8a18ee9856bbf2abc22a9fa06326cf2
Marc Hein <info@marc-hein.de>
2017-11-17 00:26:09 +0100

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,51 @@
+var express = require("express");
+const path = require('path');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('views', 'server/views');
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.redirect('/index');
+});
+
+app.get('/index', function(req, res){
+    res.render("home", {
+        title: "Startseite"
+    });
+});
+
+app.get('/zimmer', function(req, res){
+    res.render("zimmer", {
+        title: "Zimmer"
+    });
+});
+
+app.get('/guestbook', function(req, res){
+    res.render("guestbook", {
+        title: "Gästebuch"
+    });
+});
+
+app.get('/buchen', function(req, res){
+    res.render("buchen", {
+        title: "Buchen"
+    });
+});
+
+app.get('/impressum', function(req, res){
+    res.render("impressum", {
+        title: "Impressum"
+    });
+});
+
+// Handle 404 error.
+app.use("*", function (req, res) {
+	res.status(404).send("<h1>Error 404 - not found</h1>"); // not found error
+});
+
+app.listen(port, function () {
+	console.log("Server running on port: " + port);
+});
