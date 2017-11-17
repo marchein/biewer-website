c4f0bfdd70937d01b46738165241a32f00757fe2
Marc Hein <info@marc-hein.de>
2017-11-17 13:35:53 +0100

    Lightbox works & app.js is eslint conform

--- server/app.js
+++ server/app.js
@@ -1,46 +1,49 @@
 var express = require("express");
-const path = require('path');
+const path = require("path");
 var app = express();
 var port = process.env.PORT || 61015;
-var public = path.join(__dirname, '/../public')
+var publicFolder = path.join(__dirname, "/../public");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+if (app.get("env") === "development") {
+	app.locals.pretty = true;
+}
 
-app.use('/static', express.static(public));
+app.use("/static", express.static(publicFolder));
 
-app.get('/', function(req, res) {
-    res.redirect('/index');
+app.get("/", function (req, res) {
+	res.redirect("/index");
 });
 
-app.get('/index', function(req, res){
-    res.render("home", {
-        title: "Startseite"
-    });
+app.get("/index", function (req, res) {
+	res.render("home", {
+		title: "Startseite"
+	});
 });
 
-app.get('/zimmer', function(req, res){
-    res.render("zimmer", {
-        title: "Zimmer"
-    });
+app.get("/zimmer", function (req, res) {
+	res.render("zimmer", {
+		title: "Zimmer"
+	});
 });
 
-app.get('/guestbook', function(req, res){
-    res.render("guestbook", {
-        title: "Gästebuch"
-    });
+app.get("/guestbook", function (req, res) {
+	res.render("guestbook", {
+		title: "Gästebuch"
+	});
 });
 
-app.get('/buchen', function(req, res){
-    res.render("buchen", {
-        title: "Buchen"
-    });
+app.get("/buchen", function (req, res) {
+	res.render("buchen", {
+		title: "Buchen"
+	});
 });
 
-app.get('/impressum', function(req, res){
-    res.render("impressum", {
-        title: "Impressum"
-    });
+app.get("/impressum", function (req, res) {
+	res.render("impressum", {
+		title: "Impressum"
+	});
 });
 
 // Handle 404 error.
